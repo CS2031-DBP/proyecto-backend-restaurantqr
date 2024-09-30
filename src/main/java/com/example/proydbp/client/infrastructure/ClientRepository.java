52590328ea2aee136ab96d5662916406c7d6d563
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-09-30 14:57:22 -0500

    ClientRepository

--- src/main/java/com/example/proydbp/client/infrastructure/ClientRepository.java
+++ src/main/java/com/example/proydbp/client/infrastructure/ClientRepository.java
@@ -3,5 +3,8 @@ package com.example.proydbp.client.infrastructure;
 import com.example.proydbp.client.domain.Client;
 import com.example.proydbp.user.infrastructure.BaseUserRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends BaseUserRepository<Client> {
+    Optional<Client> findByEmail(String email);
 }
