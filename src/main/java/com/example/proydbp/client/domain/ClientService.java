e56b3415d1adda24ae688c3d802f4c40935921db
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-09-30 14:39:38 -0500

    ClientService

--- src/main/java/com/example/proydbp/client/domain/ClientService.java
+++ src/main/java/com/example/proydbp/client/domain/ClientService.java
@@ -1,7 +1,59 @@
 package com.example.proydbp.client.domain;
 
+import com.example.proydbp.client.dto.ClientDto;
+import com.example.proydbp.client.infrastructure.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
+
+
+    final private ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public boolean clientExists(ClientDto clientDto) {
+        Optional<Client> existingClient = clientRepository.findByEmail(clientDto.getEmail());
+        return existingClient.isPresent();
+    }
+
+    public String saveClientDto(ClientDto clientDto) {
+        Client client = new Client();
+        client.setFirstName(clientDto.getFirstName());
+        client.setEmail(clientDto.getEmail());
+        client.setPhoneNumber(clientDto.getPhoneNumber());
+        clientRepository.save(client);
+
+        return clientDto.toString();
+    }
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public void deleteClient(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+    public void updateClient(Long id, ClientDto clientDto) {
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("Cliente con id "+ id +"no encontrado."));
+
+        client.setFirstName(clientDto.getFirstName());
+        client.setEmail(clientDto.getEmail());
+        client.setPhoneNumber(clientDto.getPhoneNumber());
+        clientRepository.save(client);
+    }
+
+    public Client getClient(Long id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("Cliente con id "+ id +"no encontrado."));
+    }
 }
