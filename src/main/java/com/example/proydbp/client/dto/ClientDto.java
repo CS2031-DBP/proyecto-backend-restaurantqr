02e6bcec01bee6529f09014f28d900791302de18
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-09-30 13:59:39 -0500

    PackageUser

--- src/main/java/com/example/proydbp/client/dto/ClientDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.proydbp.client.dto;
-
-import lombok.Data;
-
-@Data
-public class ClientDto {
-}
--- src/main/java/com/example/proydbp/client/infrastructure/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.proydbp.client.infrastructure;
-
-import com.example.proydbp.client.domain.Client;
-import com.example.proydbp.user.infrastructure.BaseUserRepository;
-
-public interface ClientRepository extends BaseUserRepository<Client> {
-}
