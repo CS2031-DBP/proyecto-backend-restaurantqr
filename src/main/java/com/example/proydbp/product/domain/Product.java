320aa68a9e85cc8590283259086000237fa7d515
fmunoz <fernando.munoz.p@utec.edu.pe>
2024-10-01 02:45:08 -0500

    PackageProduct

---
+++ src/main/java/com/example/proydbp/product/domain/Product.java
@@ -0,0 +1,14 @@
+package com.example.proydbp.product.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+}
---
+++ src/main/java/com/example/proydbp/product/domain/ProductService.java
@@ -0,0 +1,7 @@
+package com.example.proydbp.product.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+}
